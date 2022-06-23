e0ee514808da41c2e9e362805d9900a4c680a3ae
AliVik <85441270+AliVik@users.noreply.github.com>
2022-06-23 16:10:00 +0300

    dd updateAvatar controller

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,32 @@
-const updateAvatar = async (req, res, next) => {
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp");
+const { findUserAndUpdate } = require("../../services/users");
 
-    
+const avatarsDir = path.join(__dirname, "../../", "public");
+
+const updateAvatar = async (req, res) => {
+  const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const avatarURL = path.join("avatars", `${id}${path.extname(originalname)}`);
+
+  try {
+    await Jimp.read(tempUpload)
+      .then((img) => {
+        return img.resize(250, 250).write(path.join(avatarsDir, avatarURL));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    await findUserAndUpdate(id, { avatarURL });
+    res.status(200).json({ avatarURL });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await fs.unlink(tempUpload);
+  }
 };
 
 module.exports = updateAvatar;
